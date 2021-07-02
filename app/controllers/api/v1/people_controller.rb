775ad02aaebac8671457b5b6f9420ce4ef867702
Fanan Dala <fanan.dala@yahoo.com>
2021-07-02 02:40:27 +0100

    feat: add REST get people

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,8 +1,12 @@
 module Api
-    module v1
+    module V1
         class PeopleController < ApplicationController
-            def index()
-                @people = Person.order('created_at DESC');
+            def index
+                search = params.has_key?('search') && params[:search] ? params[:search] : ''
+                limit = params.has_key?('limit') && params[:limit] ? params[:limit] : 30
+                page = params.has_key?('page') && params[:page] ? params[:page] : 1
+                @people = Person.where('LOWER(name) LIKE LOWER(:search) OR LOWER(email) LIKE LOWER(:search)', search: "%#{search}%").order('created_at DESC').paginate(:per_page => limit, :page => page)
+                render json: {status: 'success', message: 'People retrieved successfully', data: @people}, :except =>  [:password_digest, :id], status: :ok
             end
         end
     end
